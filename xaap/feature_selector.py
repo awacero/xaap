6eb9dc439a0f5787d1cbc63b1c2d54ccc4f4f351
acerowilson <acerowilson@gmail.com>
2021-10-06 14:06:43 -0500

    added select features and train model

--- xaap/feature_selector.py
+++ xaap/feature_selector.py
@@ -10,7 +10,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from datetime import datetime
 import json 
-
+import get_mseed_data.get_mseed_utils as gmutils
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
@@ -20,93 +20,113 @@ from numpy import average
 
 plt.rcParams["figure.figsize"]=[20,10] 
 pd.set_option('display.max_columns',None)
-data_file="./feature_tungurahua_igepn_2011_2021.csv"
-column_names = ['id_code','seismic_type']
-unique_id = datetime.now().strftime("%Y%m%d%H%M%S")
 
-data = pd.read_csv(data_file)
+def main():
+    
+    is_error = False
 
-rows_length,column_length = data.shape
+    if len(sys.argv)==1:
+        is_error = True
 
-for i in range(column_length - 2):
-    column_names.append("f_%s" %i)
-
-data.columns = column_names
-data.columns = data.columns.str.replace(' ', '')
-
-#Use just 2 types of events
-#data = data.loc[(data.seismic_type.str.contains('LP')) | (data.seismic_type.str.contains('VT')) ]
-data['seismic_type'].hist() 
-
-#Scale the features
-x_no_scaled = data.iloc[:,2:].to_numpy()
-scaler = StandardScaler()
-x = scaler.fit_transform(x_no_scaled)
+    else:
+        unique_id = datetime.now().strftime("%Y%m%d%H%M%S")
+        try:
+            run_param = gmutils.read_parameters(sys.argv[1])
+        except Exception as e:
+            raise Exception("Error reading configuration file: %s" %str(e))
 
 
-##CATEGORIZAR EN ONE HOT
-"""
-from sklearn.preprocessing import OneHotEncoder
-cat_encoder = OneHotEncoder()
+        try:
+            volcano = run_param['ENVIRONMENT']['volcano']
+            jobs = int(run_param['ENVIRONMENT']['jobs'])
+            features_file = run_param['ENVIRONMENT']['features_file']
+            features_selected_folder = run_param['ENVIRONMENT']['features_selected_folder']
+            max_features = int(run_param['ENVIRONMENT']['max_features'])
 
-data_seismic_type = data[['seismic_type']]
-data_seismic_type_onehot = cat_encoder.fit_transform(data_seismic_type)
-print(data_seismic_type_onehot.toarray())
-y = data_seismic_type_onehot.toarray()
-print((y.shape))
-"""
-##CATEGORIZAR ORDINAL ENCODER
-from sklearn.preprocessing import OrdinalEncoder
-ordinal_encoder = OrdinalEncoder()
-data_seismic_type = data[['seismic_type']]
-data_seismic_type_encode = ordinal_encoder.fit_transform(data_seismic_type)
-y = data_seismic_type_encode
-#print(y)
-print((y.shape))
+        except Exception as e:
+            raise Exception("Error reading parameters file: %s" %str(e))
 
-data_scaled_categorized = pd.DataFrame(x,columns=data.columns[2:])
+        try:
+            column_names = ['id_code','seismic_type']
+            data = pd.read_csv(features_file)
 
-data_scaled_categorized['seismic_type'] = data_seismic_type_encode
+            rows_length,column_length = data.shape
 
-data_scaled_categorized.head()
+            for i in range(column_length - 2):
+                column_names.append("f_%s" %i)
 
+            data.columns = column_names
+            data.columns = data.columns.str.replace(' ', '')
 
+            #Use just 2 types of events
+            #data = data.loc[(data.seismic_type.str.contains('LP')) | (data.seismic_type.str.contains('VT')) ]
+            data['seismic_type'].hist() 
 
-"""
-sffs = SFS (svm.SVC(), k_features=90,forward=True, floating=False,verbose=2, scoring='f1', cv=0, n_jobs=-1)
-sffs = SFS (svm.SVC())
-
-sffs.fit(x,y)
-sffs.k_feature_names_
-
-"""
-
-#'''
-##84 features x 16 cores = 32 minutes
-#scoring = F1 for binary
-#
-knn = KNeighborsClassifier(n_neighbors = 3)
-sfs = SFS(knn, k_features=50,forward=True,floating=False,verbose=2,scoring='accuracy',cv=0, n_jobs=-1)
-sfs.fit(x, y.ravel())
-
-#'''
+            #Scale the features
+            x_no_scaled = data.iloc[:,2:].to_numpy()
+            scaler = StandardScaler()
+            x = scaler.fit_transform(x_no_scaled)
 
 
+            ##CATEGORIZAR EN ONE HOT
+            """
+            from sklearn.preprocessing import OneHotEncoder
+            cat_encoder = OneHotEncoder()
+
+            data_seismic_type = data[['seismic_type']]
+            data_seismic_type_onehot = cat_encoder.fit_transform(data_seismic_type)
+            print(data_seismic_type_onehot.toarray())
+            y = data_seismic_type_onehot.toarray()
+            print((y.shape))
+            """
+            ##CATEGORIZAR ORDINAL ENCODER
+            from sklearn.preprocessing import OrdinalEncoder
+            ordinal_encoder = OrdinalEncoder()
+            data_seismic_type = data[['seismic_type']]
+            data_seismic_type_encode = ordinal_encoder.fit_transform(data_seismic_type)
+            y = data_seismic_type_encode
+
+            data_scaled_categorized = pd.DataFrame(x,columns=data.columns[2:])
+            data_scaled_categorized['seismic_type'] = data_seismic_type_encode
+
+            data_scaled_categorized.head()
+
+            """
+            sffs = SFS (svm.SVC(), k_features=90,forward=True, floating=False,verbose=2, scoring='f1', cv=0, n_jobs=-1)
+            sffs = SFS (svm.SVC())
+
+            sffs.fit(x,y)
+            sffs.k_feature_names_
+
+            """
+
+            #'''
+            ##84 features x 16 cores = 32 minutes, 30
+            #scoring = F1 for binary
+            #
+            knn = KNeighborsClassifier(n_neighbors = 3)
+            sfs = SFS(knn, k_features=max_features,forward=True,floating=False,verbose=2,scoring='accuracy',cv=0, n_jobs=jobs)
+            sfs.fit(x, y.ravel())
+
+            #'''
+
+            ##Plotear los resultados 
+            fig1 = plot_sfs(sfs.get_metric_dict(), kind='std_dev') 
+            plt.title('Sequential Forward Selection (w. StdErr)') 
+            plt.grid() 
+            plt.savefig("%s/%s_best_features_%s.png" %(features_selected_folder, volcano, unique_id) )
+            #print(sfs.k_feature_names_)
+
+            best_features_pd = pd.DataFrame.from_dict(sfs.get_metric_dict()).T
+            best_features_pd.to_csv("%s/%s_best_features_%s.csv" %(features_selected_folder, volcano, unique_id))
 
 
-fig1 = plot_sfs(sfs.get_metric_dict(), kind='std_dev') 
-plt.title('Sequential Forward Selection (w. StdErr)') 
-plt.grid() 
-#plt.show()
-plt.savefig("./best_features_%s.png" %unique_id) 
-#print(sfs.k_feature_names_)
+        except Exception as e:
+            print("Error in selecting features was: %s" %str(e))
 
-best_features_pd = pd.DataFrame.from_dict(sfs.get_metric_dict()).T
-best_features_pd.to_csv("./best_features_%s.csv" %unique_id)
-"""
-print(type(sfs.subsets_))
-print((sfs.subsets_))
-best_features_file = open("./best_features_%s.json" %unique_id,"w")
-best_features_file.write(str(sfs.subsets_))
-"""
+    if is_error:
+        print(f'Usage: python {sys.argv[0]} profile_select_feature_xxxx.txt ')    
 
+
+if __name__ == "__main__": 
+    main()
---
+++ xaap/random_forest_train_model.py
@@ -0,0 +1,112 @@
+'''
+Created on Oct 1, 2021
+
+@author: wacero
+'''
+import sys,os
+from datetime import datetime
+from get_mseed_data import get_mseed_utils as gmutils
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import pickle
+
+
+def main():
+    is_error =  False
+
+    if len(sys.argv)==1:
+        is_error = True
+        
+    else:
+        unique_id = datetime.now().strftime("%Y%m%d%H%M%S")
+        try:
+            run_param = gmutils.read_parameters(sys.argv[1])
+        except Exception as e:
+            raise Exception("Error reading configuration file: %s" %str(e))
+
+        try:
+            volcano = run_param['ENVIRONMENT']['volcano']
+            features_file = run_param['ENVIRONMENT']['features_file']
+            model_folder = run_param['ENVIRONMENT']['model_folder']
+
+        except Exception as e:
+            raise Exception("Error reading parameters file: %s" %str(e))
+        
+        try:
+            column_names = ['id_code','seismic_type']
+            data = pd.read_csv(features_file)
+
+            rows_length,column_length = data.shape
+
+            for i in range(column_length - 2):
+                column_names.append("f_%s" %i)
+
+            data.columns = column_names
+            data.columns = data.columns.str.replace(' ', '')
+
+            #Use just 2 types of events
+            #data = data.loc[(data.seismic_type.str.contains('LP')) | (data.seismic_type.str.contains('VT')) ]
+            data['seismic_type'].hist() 
+
+            x_no_scaled = data.iloc[:,2:].to_numpy()
+            scaler = StandardScaler()
+            x = scaler.fit_transform(x_no_scaled)
+            data_scaled = pd.DataFrame(x,columns=data.columns[2:])
+
+            ##CATEGORIZAR ORDINAL ENCODER
+            from sklearn.preprocessing import OrdinalEncoder
+            ordinal_encoder = OrdinalEncoder()
+            data_seismic_type = data[['seismic_type']]
+            data_seismic_type_encode = ordinal_encoder.fit_transform(data_seismic_type)
+
+
+            ##CHOSE BEST FEATURES 
+            best_features =  [2, 3, 6, 8, 9, 11, 23, 26, 27, 29, 42, 44, 45, 47, 48, 50, 51, 52, 53, 63, 67, 71, 72, 73, 75, 95]
+            #data_scaled_best = data_scaled.iloc[:,best_features]
+            data_scaled_best = data_scaled.iloc[:,:]
+
+            data_scaled_best['seismic_type'] = data_seismic_type_encode
+
+            print(data_scaled_best.head())
+
+
+
+
+            train_data, test_data = train_test_split(data_scaled_best,test_size=0.2,random_state=42)
+
+            x_train = train_data.loc[:,train_data.columns !='seismic_type']
+            y_train = train_data.loc[:,train_data.columns =='seismic_type']
+
+            x_test = test_data.loc[:,test_data.columns !='seismic_type']
+            y_test = test_data.loc[:,test_data.columns =='seismic_type']
+
+            
+
+            rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1)
+            rnd_clf.fit(x_train,y_train)
+
+            y_pred = rnd_clf.predict(x_test)
+            print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+            model_path=os.path.join("./" ,"%s" %model_folder)
+
+            if not os.path.exists(model_path):
+                os.makedirs(model_path)
+
+            pickle.dump(rnd_clf, open(os.path.join(model_path,'%s_rf_%s.pkl'%(volcano,unique_id)),'wb'), protocol=4)
+
+
+        except Exception as e:
+            print("Error in training model was: %s" %str(e))     
+
+    if is_error:
+        print(f'Usage: python {sys.argv[0]} profile_train_model_xxxx.txt ')    
+
+
+if __name__ == "__main__": 
+    main()
