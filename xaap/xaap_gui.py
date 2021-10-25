887a93b84f55f1e7d02501ba69b9edf5e3ec9cc0
acerowilson <acerowilson@gmail.com>
2021-10-25 11:44:17 -0500

    add config files

--- xaap/xaap_gui.py
+++ xaap/xaap_gui.py
@@ -18,6 +18,12 @@ from obspy.signal.trigger import classic_sta_lta
 from obspy.signal.trigger import classic_sta_lta, classic_sta_lta_py, recursive_sta_lta_py, plot_trigger, trigger_onset
 from datetime import date, datetime
 
+import pandas as pd
+from aaa_features.features import FeatureVector 
+from sklearn.preprocessing import StandardScaler
+import pickle 
+from sklearn.ensemble import RandomForestClassifier
+
 import logging, logging.config
 
 xaap_dir = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])))
@@ -51,7 +57,10 @@ class xaapGUI(QtGui.QWidget):
         self.params.param('Plot stream').sigActivated.connect(self.plot_stream)
         self.window_region.sigRegionChanged.connect(self.set_p1_using_p2)
         self.p1.sigRangeChanged.connect(self.set_p2_using_p1)
-        
+        self.params.param('Detect triggers').sigActivated.connect(self.detect_triggers)
+        self.params.param('Classify triggers').sigActivated.connect(self.classify_triggers)
+
+        '''Start of automatic process '''
         self.request_stream()
 
         if self.stream == None:
@@ -63,21 +72,14 @@ class xaapGUI(QtGui.QWidget):
             logger.info("ok request_stream() in __init__. Continue with Pre-process")
             self.pre_process_stream()
             self.plot_stream()
+            self.detect_triggers()
+            #self.classify_triggers()
             #self.window_region.sigRegionChanged.connect(self.set_p1_using_p2)
             
 
 
 
 
-        ''' 
-        ##Hacer que cambie la region al cambiar el valor maximo
-        min_region=0
-        max_region=int(len(self.times)*0.1)
-        self.window_region.setRegion([self.times[min_region], self.times[max_region]])  
-        self.p1.sigRangeChanged.connect(self.update_region)
-
-
-        '''
 
     def create_parameters(self):
         
@@ -121,13 +123,13 @@ class xaapGUI(QtGui.QWidget):
                                                           ]},
             {'name':'Request Data','type':'action'},                                                           
             {'name':'Pre-process','type':'action'},
-            {'name':'Plot stream','type':'action'}              
+            {'name':'Plot stream','type':'action'},
+            {'name':'Detect triggers','type':'action'},
+            {'name':'Classify triggers','type':'action'}                
                                                                          ]                                                                         
                                                                          )
         logger.info("End of set parameters") 
         return xaap_parameters
-
-
 
 
     def request_stream(self):
@@ -155,45 +157,160 @@ class xaapGUI(QtGui.QWidget):
 
         try:
             temp_mseed = get_mseed.get_stream(mseed_client_id,self.mseed_client,network,station,'',channel,start_time=start_time,end_time=end_time)
-            logger.info("request_stream() ok: %s" %temp_mseed)
+            logger.info("request_stream() result was: %s" %temp_mseed)
             self.stream = temp_mseed
 
         except Exception as e:
             logger.error("Error in request_stream was: %s" %str(e))
 
 
+    def pre_process_stream(self):
 
-    def save(self):
-        filename = pg.QtGui.QFileDialog.getSaveFileName(self, "Save State..", "xaap_configuracion.cfg", "Config Files (*.cfg)")
-        if isinstance(filename, tuple):
-            filename = filename[0]  # Qt4/5 API difference
-        if filename == '':
-            return
-        state = self.params.saveState()
-        pg.configfile.writeConfigFile(state, str(filename)) 
+        self.processed_stream = None
+        logger.info("self.processed_stream cleaned :%s" %self.processed_stream)
+        try:
+            self.processed_stream = self.stream.copy()
+            self.processed_stream.merge(method=1, fill_value="interpolate",interpolation_samples=0)
+            logger.info("Stream merged: %s" %self.processed_stream)
+
+            self.times = self.processed_stream[0].times(type="timestamp")
+            sampling_rate = self.processed_stream[0].stats.sampling_rate
+
+            f_a = self.params['Parameters','Filter','Freq_A']
+            f_b = self.params['Parameters','Filter','Freq_B']
+
+            filter_type = self.params['Parameters','Filter','Filter type']
+            if filter_type == 'highpass':
+                logger.info("highpass selected")
+                temp_data = filter.highpass(self.processed_stream[0].data,f_a,sampling_rate,4)
+            elif filter_type == 'bandpass':
+                logger.info("bandpass selected")
+                temp_data = filter.bandpass(self.processed_stream[0].data,f_a,f_b,sampling_rate,4)
+            elif filter_type == 'lowpass':
+                logger.info("lowpass selected")
+                temp_data = filter.lowpass(self.processed_stream[0].data,f_a,sampling_rate,4)
+
+            self.processed_stream[0].data = temp_data
+
+        except Exception as e:
+            logger.error("Error at pre_process_stream() was : %s" %str(e))
 
 
-    def loadPreset(self,param,preset):
+    def plot_stream(self):
 
-        print("###%s, %s" %(param,preset))
-        if preset == '':
-            return
-        fn = os.path.join(self.parameters_dir, preset+".cfg")
-        print(fn)
-        state = pg.configfile.readConfigFile(fn)
-        self.loadState(state)
+        logger.info("start plot_stream(). Clean plots")
+        self.p1.clearPlots()
+        self.p2.clearPlots()
 
-    def loadState(self, state):
-        if 'Load Preset..' in state['children']:
-            del state['children']['Load Preset..']['limits']
-            del state['children']['Load Preset..']['value']
-        self.params.param('Parameters').clearChildren()
-        self.params.restoreState(state, removeChildren=False)
+        try:
 
-        ##llamar a ploteo 
-        #self.pre_process_stream()
+            logger.info("Plot in p1: %s" %self.processed_stream)
+            self.p1.plot(self.times,self.processed_stream[0].data,pen='g')
+            logger.info("Plot in p2")
+            self.p2d = self.p2.plot(self.times,self.processed_stream[0].data,pen="w")
+            
+            min_region=0
+            max_region=int(len(self.times)*self.params['Parameters','GUI','zoom_region_size'])
+            self.window_region.setRegion([self.times[min_region], self.times[max_region]])
+            self.p1.setXRange(self.times[min_region], self.times[max_region])
+
+            logger.info("End plot_stream()")
+
+        except Exception as e:
+
+            logger.error("Error in plot_stream() was: %s" %str(e))
 
 
+    def detect_triggers(self):
+
+        sta = self.params['Parameters','STA_LTA','sta']
+        lta = self.params['Parameters','STA_LTA','lta']
+        trigon = self.params['Parameters','STA_LTA','trigon']
+        trigoff = self.params['Parameters','STA_LTA','trigoff']
+
+        triggers_on = []
+        scan_window_size = 600
+        start_pad = 30
+        end_pad = 30
+        sampling_rate = self.processed_stream[0].stats.sampling_rate
+        start_time = self.processed_stream[0].stats.starttime
+        end_time = self.processed_stream[0].stats.endtime
+
+        n_windows = int(np.ceil(end_time - start_time)/scan_window_size) 
+        self.triggers_traces = []
+        triggers_on = []
+        trigger_dot_list=[]
+        for i in range(n_windows):
+            window_start = start_time + scan_window_size*i - start_pad
+            window_end = start_time + scan_window_size*(i + 1) + start_pad
+            
+            temp_trace = self.processed_stream[0].slice(window_start,window_end)
+
+            cft = classic_sta_lta(temp_trace.data, int(sta * sampling_rate), int(lta * sampling_rate))
+
+            on_off=trigger_onset(cft, trigon, trigoff)
+
+            if len(on_off) != 0:
+                for time in on_off:
+                    #print(time)
+                    time_on = int(time[0]/sampling_rate)
+                    time_off = int(time[1]/sampling_rate)
+
+                    triggers_on.append((window_start + time_on).timestamp)
+                    trigger_dot_list.append(0)
+
+                    trigger_trace = temp_trace.slice(window_start + time_on, window_start + time_off)
+                    self.triggers_traces.append(trigger_trace)
+        
+        #print(triggers_on,trigger_dot_list)
+        self.p1.plot(triggers_on,trigger_dot_list,pen=None, symbol='x')
+        self.p2.plot(triggers_on,trigger_dot_list,pen=None, symbol='x')
+
+    def classify_triggers(self):
+
+        feature_config = {"features_file":"%s/config/features/features_00.json" %self.xaap_dir,
+                    "domains":"time spectral cepstral"}
+        features = FeatureVector(feature_config, verbatim=2)
+        tungu_clf= pickle.load(open(os.path.join('%s/data/models' %self.xaap_dir,'tungurahua_rf_20211007144655.pkl'),'rb'))
+        print(tungu_clf)
+        categories = [' BRAM ', ' CRD ', ' EXP ', ' HB ', ' LH ', ' LP ', ' TRARM ', ' TREMI ', ' TRESP ', ' VT ']
+
+        input_data = []
+
+        logger.info("start feature calculation")
+        for trace in self.triggers_traces:
+            print(trace)
+            file_code = "%s.%s.%s.%s.%s" %(trace.stats.network,trace.stats.station,trace.stats.location,trace.stats.channel,trace.stats.starttime.strftime("%Y.%m.%d.%H%M%S"))
+            features.compute(trace.data,trace.stats.sampling_rate)
+            row = np.append(file_code, features.featuresValues)
+            input_data.append(row)
+
+            column_names = ['data_code']
+            data = pd.DataFrame(input_data)
+            rows_length,column_length = data.shape
+
+            for i in range(column_length - 1):
+                column_names.append("f_%s" %i)
+
+            data.columns = column_names
+            data.columns = data.columns.str.replace(' ', '')
+
+            x_no_scaled = data.iloc[:,1:].to_numpy()
+
+            scaler = StandardScaler()
+            x = scaler.fit_transform(x_no_scaled)
+            data_scaled = pd.DataFrame(x,columns=data.columns[1:])
+
+            print(data_scaled.shape)
+
+            y_pred=tungu_clf.predict(data_scaled)
+
+            print(type(y_pred))
+            print(y_pred.shape)
+
+            for i in range(rows_length):
+                prediction = "%s,%s" %(data.iloc[i,0],categories[int(y_pred[i])])
+                logger.info(prediction)
     def setupGUI(self):
 
         self.layout = QtGui.QVBoxLayout()
@@ -223,84 +340,39 @@ class xaapGUI(QtGui.QWidget):
         self.window_region = pg.LinearRegionItem()
         self.p2.addItem(self.window_region, ignoreBounds=True)        
 
+    def save(self):
+        filename = pg.QtGui.QFileDialog.getSaveFileName(self, "Save State..", "xaap_configuracion.cfg", "Config Files (*.cfg)")
+        if isinstance(filename, tuple):
+            filename = filename[0]  # Qt4/5 API difference
+        if filename == '':
+            return
+        state = self.params.saveState()
+        pg.configfile.writeConfigFile(state, str(filename)) 
+
+    def loadPreset(self,param,preset):
+
+        if preset == '':
+            return
+        fn = os.path.join(self.parameters_dir, preset+".cfg")
+        state = pg.configfile.readConfigFile(fn)
+        self.loadState(state)
+
+    def loadState(self, state):
+        if 'Load Preset..' in state['children']:
+            del state['children']['Load Preset..']['limits']
+            del state['children']['Load Preset..']['value']
+        self.params.param('Parameters').clearChildren()
+        self.params.restoreState(state, removeChildren=False)
+
+        ##llamar a ploteo 
+        #self.pre_process_stream()
 
 
-    def pre_process_stream(self):
 
-        self.processed_stream = None
-        logger.info("self.processed_stream cleaned :%s" %self.processed_stream)
-        try:
-            self.processed_stream = self.stream.copy()
-            self.processed_stream.merge(method=1, fill_value="interpolate",interpolation_samples=0)
-            logger.info("Stream merged: %s" %self.processed_stream)
-
-            self.times = self.processed_stream[0].times(type="timestamp")
-
-            print("times original")
-            print(len(self.times))
-            print(len(self.processed_stream[0].data))
-
-            sampling_rate = self.processed_stream[0].stats.sampling_rate
-
-            f_a = self.params['Parameters','Filter','Freq_A']
-            f_b = self.params['Parameters','Filter','Freq_B']
-
-            filter_type = self.params['Parameters','Filter','Filter type']
-            if filter_type == 'highpass':
-                logger.info("highpass selected")
-                temp_data = filter.highpass(self.processed_stream[0].data,f_a,sampling_rate,4)
-            elif filter_type == 'bandpass':
-                logger.info("bandpass selected")
-                temp_data = filter.bandpass(self.processed_stream[0].data,f_a,f_b,sampling_rate,4)
-            elif filter_type == 'lowpass':
-                logger.info("lowpass selected")
-                temp_data = filter.lowpass(self.processed_stream[0].data,f_a,sampling_rate,4)
-
-            
-            self.processed_stream[0].data = temp_data
-            
-            print("times filtered")
-            print(len(self.processed_stream[0].times(type="timestamp")))
-            print(len(self.processed_stream[0].data))
-
-        except Exception as e:
-            logger.error("Error at pre_process_stream() was : %s" %str(e))
-
-
-    def plot_stream(self):
-
-        logger.info("start plot_stream(). Clean plots")
-        self.p1.clear()
-        self.p2.clearPlots()
-
-        try:
-
-            logger.info("Plot in p1: %s" %self.processed_stream)
-            self.p1.plot(self.times,self.processed_stream[0].data,pen='g')
-            logger.info("Plot in p2")
-            self.p2d = self.p2.plot(self.times,self.processed_stream[0].data,pen="w")
-            #logger.info("Set clip item") ##Not needed??
-            #self.window_region.setClipItem(self.p2d)
-            
-            min_region=0
-            max_region=int(len(self.times)*self.params['Parameters','GUI','zoom_region_size'])
-            self.window_region.setRegion([self.times[min_region], self.times[max_region]])
-            self.p1.setXRange(self.times[min_region], self.times[max_region])
-
-            logger.info("End plot_stream()")
-
-        except Exception as e:
-
-            logger.error("Error in plot_stream() was: %s" %str(e))
 
     def set_p1_using_p2(self):
         
         minX,maxX = self.window_region.getRegion()
-        
-        t_min = datetime.utcfromtimestamp(minX)
-        t_max = datetime.utcfromtimestamp(maxX)
-        #logger.info("update_region_size() %s,%s" %(t_min,t_max))
-
         self.p1.setXRange(minX,maxX,padding=0)
 
 
@@ -310,73 +382,6 @@ class xaapGUI(QtGui.QWidget):
         self.window_region.setRegion(rgn)
 
 
-
-    def update_region(self,window, viewRange):
-        rgn = viewRange[0]
-        self.region.setRegion(rgn)
-
-        t_min,t_max= self.region.getRegion()
-        t_max = datetime.utcfromtimestamp(t_max)
-        t_min = datetime.utcfromtimestamp(t_min)
-        logger.info("update_region() %s,%s" %(t_min,t_max))
-
-    def detect_event(self):
-
-        sta = self.params['Parameters','STA_LTA','sta']
-        lta = self.params['Parameters','STA_LTA','lta']
-        trigon = self.params['Parameters','STA_LTA','trigon']
-        trigoff = self.params['Parameters','STA_LTA','trigoff']
-
-
-
-        triggers_on = []
-        scan_window_size = 600
-        start_pad = 30
-        end_pad = 30
-        sampling_rate = self.stream[0].stats.sampling_rate
-        start_time = self.stream[0].stats.starttime
-        end_time = self.stream[0].stats.endtime
-
-        n_windows = int(np.ceil(end_time - start_time)/scan_window_size) 
-
-        triggers_on = []
-        trigger_dot_list=[]
-        for i in range(n_windows):
-            window_start = start_time + scan_window_size*i - start_pad
-            window_end = start_time + scan_window_size*(i + 1) + start_pad
-            
-            temp_trace = self.stream[0].slice(window_start,window_end)
-
-            #print(temp_trace)
-
-            cft = classic_sta_lta(temp_trace.data, int(sta * sampling_rate), int(lta * sampling_rate))
-
-            on_off=trigger_onset(cft, trigon, trigoff)
-
-            if len(on_off) != 0:
-                for time in on_off:
-                    #print(time)
-                    time_on = int(time[0]/sampling_rate)
-                    time_off = int(time[1]/sampling_rate)
-
-                    triggers_on.append((window_start + time_on).timestamp)
-                    trigger_dot_list.append(0)
-        
-        #print(triggers_on,trigger_dot_list)
-        self.p1.plot(triggers_on,trigger_dot_list,pen=None, symbol='x')
-        self.p2.plot(triggers_on,trigger_dot_list,pen=None, symbol='x')
-
-
-    def read_stream(self):
-        
-        '''Return a obspy stream object'''
-
-        try:
-            data_file="/home/wacero/proyectos_codigo/xaap/xaap/data/EC.RETU..SHZ.D.2012.187"
-            stream = read(data_file)     
-            logger.info("End of read_stream: %s" %self.stream)
-        except Exception as e:
-            raise("Error in read_stream was: %s" %str(e))
 
 
 
