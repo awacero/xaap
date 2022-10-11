bfa26332d7b8fd64b75f6a816968ccacec97649b
Jorge Pérez Chimborazo <102699851+JorgePerezEC@users.noreply.github.com>
2022-10-11 09:53:15 -0500

    Correction in the path of the log configuration file

--- xaap/test/test_xaap_cli.py
+++ xaap/test/test_xaap_cli.py
@@ -1,14 +1,16 @@
-from ast import Import
 import xaap_cli
 import pytest
 
-filter_json = {
-    "name":"STATION_A",
-    "type":"None",
-    "frequency_1":"None",
-    "frequency_2":"None",
-    "order":"None"
-            }
+filter_json2 = {
+    "name": "STATION_A",
+    "type": "None",
+    "frequency_1": "None",
+    "frequency_2": "None",
+    "order": "None"
+}
+
+filter_json = {'filter_0': {'name': 'rms', 'type': 'None', 'frequency_1': 'None', 'frequency_2': 'None', 'order': 'None'}, 'Freq_A': {'name': 'banda1', 'type': 'bandpass', 'frequency_1': '0.5', 'frequency_2': '0.1'}, 'Freq_B': {'name': 'banda2', 'type': 'bandpass', 'frequency_1': '2', 'frequency_2': '8'}, 'filter_4': {'name': 'banda4', 'type': 'highpass', 'frequency_1': '10.0', 'frequency_2': 'None', 'order': '1', 'zerophase': 'True'}}
+
 
 
 def test_create_filter_list():
